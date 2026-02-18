b7ad62b78f1b2981d586153bf9b72b95aa17a0c1
nevil18 <nevilanghan2005@gmail.com>
2026-02-18 22:52:50 +0530

    api

--- src/index.js
+++ src/index.js
@@ -4,3 +4,15 @@ import dotenv from "dotenv"
 dotenv.config();
 
 connetDB()
+.then(
+    () => {
+        app.listen(process.env.PORT || 8000 ,() => {
+            console.log(`Server is running on port ${process.env.PORT || 8000}`);
+        })
+    }
+)
+.catch(
+    (err) => {
+        console.log("Mongodb not connected ",err);
+    }
+)
