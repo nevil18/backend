2f551228b39c44140a9ea46a181c07e23a0d6cdd
nevil18 <nevilanghan2005@gmail.com>
2026-02-21 22:42:05 +0530

    register login logout

---
+++ src/middlewares/auth.js
@@ -0,0 +1,28 @@
+import { jwt } from "jsonwebtoken";
+import { asyncHandler } from "../utils/asyncHandler.js";
+import { ApiError } from "../utils/ApiError.js";
+import { User } from "../models/user.model.js";
+
+export const verifyJWT = asyncHandler(async (req, res, next) => {
+    try {
+        const token = req.cookies?.accessToken || req.headers("Authorization")?.replace("Bearer ", "");
+        
+        if(!token) {
+            throw new ApiError(401, "Access token missing")
+        }
+        
+        const decoded = jwt.verify(token, process.env.ACCESS_TOKEN_SECRET);
+        
+        const user = await User.findById(decoded._id).select("-password -refreshToken")
+    
+        if(!user) {
+            throw new ApiError(401, "Invalid token")
+        }
+    
+        req.user = user;
+        next();
+    } catch (error) {
+        throw new ApiError(401, error?.message || "Invalid access token")
+    }
+
+})
